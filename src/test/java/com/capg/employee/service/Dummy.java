76523ea5c2ab26dc4cbd4e55196d734a3c18550d
am35 <am35@host.docker.internal>
2022-02-01 12:57:18 +0530

    Dummy junit

---
+++ src/test/java/com/capg/employee/service/Dummy.java
@@ -0,0 +1,14 @@
+package com.capg.employee.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Dummy {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
