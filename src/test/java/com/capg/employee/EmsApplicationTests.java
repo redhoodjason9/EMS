2eca11eac6ef6b8a670fea835ba1fa65965f091f
am35 <am35@host.docker.internal>
2022-02-01 13:15:36 +0530

    dummy junit 2

--- src/test/java/com/capg/employee/EmsApplicationTests.java
+++ src/test/java/com/capg/employee/EmsApplicationTests.java
@@ -3,7 +3,7 @@ package com.capg.employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EmsApplicationTests {
 
 	@Test
